8d171c6b9403bc7bedccc1c4506aa198e33eac19
mdallas <mariosdallas2995@gmail.com>
2024-04-22 14:22:23 +0200

    preparing the template

--- StfGen/ExternalInternalStfGenerator/externalStfGenerator.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = externalStfGenerator(inputArg1,inputArg2)
-%EXTERNALSTFGENERATOR Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
