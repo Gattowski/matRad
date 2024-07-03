e34ab890c9f602d6e3cfa098b89e2d7468544ed1
mdallas <mariosdallas2995@gmail.com>
2024-07-03 22:32:37 +0300

    First (successfull???? probably not) iteration of stfGenerator classes

--- stfGen/matRad_StfGenerator.m
+++ stfGen/matRad_StfGenerator.m
@@ -1,62 +1,101 @@
-classdef (Abstract) matRad_StfGenerator    
-% Implement these properties in a subclass
+classdef matRad_StfGenerator 
+
     properties (Constant, Abstract)
        shortName;               % short identifier by which matRad recognizes an engine
        name;                    % user readable name for dose engine
        possibleRadiationModes;  % radiation modes the engine is meant to process
-       %supportedQuantities;    % supported (influence) quantities. Does not include quantities that can be derived post-calculation.
     end    
     
-    % Public properties
+ 
     properties
-        totalNumOfBixels;
-
+        ct
+        cst
+        pln
+        visMode
+        matRad_cfg
+        stf
     end
     
-    % Protected properties with public get access
-    properties (SetAccess = protected, GetAccess = public)
-        machine;                % base data defined in machine file
-    end
-
-
-    methods      
-        %Constructor  
-        function this = matRad_StfGenerator(pln)
-            this.setDefaults();
-            if nargin == 1 && ~isempty(pln)
-                this.assignPropertiesFromPln(pln);
-            end
-        end
-
-        function assignPropertiesFromPln(this,pln,warnWhenPropertyChanged)
-            matRad_cfg = MatRad_Config.instance();
-
-
-            % find all target voxels from cst cell array
-            V = [];
-            for i=1:size(cst,1)
-                if isequal(cst{i,3},'TARGET') && ~isempty(cst{i,6})
-                    V = [V; cst{i,4}{1}];
-                end
-            end
-
-            % Remove double voxels
-            V = unique(V);
-            % generate voi cube for targets
-            voiTarget    = zeros(ct.cubeDim);
-            voiTarget(V) = 1;
-    
-            % add margin
-            addmarginBool = matRad_cfg.propStf.defaultAddMargin;
-            if isfield(pln,'propStf') && isfield(pln.propStf,'addMargin')
-               addmarginBool = pln.propStf.addMargin; 
+    methods
+        function this = matRad_StfGenerator(ct, cst, pln, visMode)
+              
+            
+            this.matRad_cfg = MatRad_Config.instance();
+            addpath(fullfile(this.matRad_cfg.matRadRoot));
+            this.matRad_cfg.dispInfo('matRad: Generating stf struct...');
+            this.pln = pln;
+            this.cst = cst;
+            this.ct = ct;
+            this.visMode = visMode;
+            
+            if ~isfield(pln, 'propStf')
+                this.matRad_cfg.dispError('no applicator information in pln struct');
             end
             
+            this = this.generateImageCoordinates();
+            this = this.saveVoiCoordinates();
+            this = this.metaInfoFromPln();
+            this.matRad_cfg.dispInfo('Processing completed: %d%%\n', 100);
 
-                 
+            
         end
+        
+        function this = generateImageCoordinates(this)
+            V = [];
+            for i = 1:size(this.cst, 1)
+                if isequal(this.cst{i, 3}, 'TARGET') && ~isempty(this.cst{i, 6})
+                    V = [V; vertcat(this.cst{i, 4}{:})];
+                end
+            end
+            
+            V = unique(V);
+            voiTarget = zeros(this.ct.cubeDim);
+            voiTarget(V) = 1;
+            
+            addmarginBool = this.matRad_cfg.propStf.defaultAddMargin;
+            if isfield(this.pln, 'propStf') && isfield(this.pln.propStf, 'addMargin')
+                addmarginBool = this.pln.propStf.addMargin;
+            end
+            
+            if addmarginBool
+                voiTarget = matRad_addMargin(voiTarget, this.cst, this.ct.resolution, this.ct.resolution, true);
+                V = find(voiTarget > 0);
+            end
+            
+            if isempty(V)
+                this.matRad_cfg.dispError('Could not find target.');
+            end
+            
+            [coordsY_vox, coordsX_vox, coordsZ_vox] = ind2sub(this.ct.cubeDim, V);
+            
+            this.stf.targetVolume.Xvox = this.ct.x(coordsX_vox);
+            this.stf.targetVolume.Yvox = this.ct.y(coordsY_vox);
+            this.stf.targetVolume.Zvox = this.ct.z(coordsZ_vox);
+            
+            V = [this.cst{:, 4}];
+            V = unique(vertcat(V{:}));
+            
+            eraseCtDensMask = ones(prod(this.ct.cubeDim), 1);
+            eraseCtDensMask(V) = 0;
+            for i = 1:this.ct.numOfCtScen
+                this.ct.cube{i}(eraseCtDensMask == 1) = 0;
+            end
+        end
+        
+        function this = saveVoiCoordinates(this)
+            this.stf.targetVolume.Xvox = this.ct.x(this.stf.targetVolume.Xvox);
+            this.stf.targetVolume.Yvox = this.ct.y(this.stf.targetVolume.Yvox);
+            this.stf.targetVolume.Zvox = this.ct.z(this.stf.targetVolume.Zvox);
+        end
+        
+        
 
+        
     end
 end
+
+
+
+
 
 
--- stfGen/matRad_photonStfGenerator.m
+++ stfGen/matRad_photonStfGenerator.m
@@ -1,34 +1,41 @@
-classdef matRad_photonStfGen < ExternalInternalStfGenerator.matRad_externalStfGenerator
+classdef matRad_photonStfGenerator < matRad_externalStfGenerator
 
     properties (Constant)
         possibleRadiationModes = {'photons'};
-        name = 'photonExtStfGen';
-        shortName = 'photonExtStfGen';
-    end     
+        name = 'photonStfGen';
+        shortName = 'photonStfGen';
+    end 
+
 
     methods
+        function this = matRad_photonStfGenerator(ct, cst, pln, visMode)
+            this@matRad_externalStfGenerator(ct, cst, pln, visMode);
+        end
         
-        function this = matRad_photonStfGen(pln)   
-            if nargin < 1
-                pln = [];
+        function generateStf(this) % photon stf 
+            generateStf@matRad_externalStfGenerator(this);
+            
+            % Save ray and target position in lps system. 260-275
+            for j = 1:stf(i).numOfRays
+                         stf(i).ray(j).beamletCornersAtIso = [rayPos(j,:) + [+stf(i).bixelWidth/2,0,+stf(i).bixelWidth/2];...
+                                                              rayPos(j,:) + [-stf(i).bixelWidth/2,0,+stf(i).bixelWidth/2];...
+                                                              rayPos(j,:) + [-stf(i).bixelWidth/2,0,-stf(i).bixelWidth/2];...
+                                                              rayPos(j,:) + [+stf(i).bixelWidth/2,0,-stf(i).bixelWidth/2]]*rotMat_vectors_T;
+                         stf(i).ray(j).rayCorners_SCD = (repmat([0, machine.meta.SCD - SAD, 0],4,1)+ (machine.meta.SCD/SAD) * ...
+                                                                          [rayPos(j,:) + [+stf(i).bixelWidth/2,0,+stf(i).bixelWidth/2];...
+                                                                           rayPos(j,:) + [-stf(i).bixelWidth/2,0,+stf(i).bixelWidth/2];...
+                                                                           rayPos(j,:) + [-stf(i).bixelWidth/2,0,-stf(i).bixelWidth/2];...
+                                                                           rayPos(j,:) + [+stf(i).bixelWidth/2,0,-stf(i).bixelWidth/2]])*rotMat_vectors_T;
             end
 
-            % call superclass constructor
-            this = this@ExternalInternalStfGenerator.matRad_externalStfGenerator(pln); 
 
 
-        end
-        function setDefaults(this)
-            setDefaults@ExternalInternalStfGenerator.matRad_externalStfGenerator(this);
-
-            % create config instance
-            matRad_cfg = MatRad_Config.instance(); 
-            
+            for j = stf(i).numOfRays:-1:1 % -463
+                 % book keeping for photons
+                 stf(i).ray(j).energy = machine.data.energy;
+            end
 
             
-           
-            
-
         end
     end
 end
